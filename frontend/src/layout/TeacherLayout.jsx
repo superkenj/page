490771ff9bc046de625ccf01540d014125855d11
Kenn Jay <kennjay.bastasa@gmail.com>
2025-11-16 22:03:12 +0800

    frontend changes 19.7 - Sidebar

--- frontend/src/layout/TeacherLayout.jsx
+++ frontend/src/layout/TeacherLayout.jsx
@@ -3,22 +3,27 @@ import TeacherSidebar from "./TeacherSidebar";
 
 export default function TeacherLayout() {
   return (
-    <div style={{ display: "flex", height: "100vh", overflow: "hidden" }}>
+    <div style={{
+      display: "flex",
+      height: "100vh",
+      overflow: "hidden"
+    }}>
       
       {/* FIXED SIDEBAR */}
       <TeacherSidebar />
 
-      {/* ONLY THIS PART SHOULD SCROLL */}
+      {/* SCROLLABLE MAIN CONTENT */}
       <main
         style={{
           flex: 1,
           overflowY: "auto",
-          padding: "2rem",
           background: "#f8fbff",
+          padding: "2rem"
         }}
       >
         <Outlet />
       </main>
+
     </div>
   );
 }
